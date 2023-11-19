b7955bf5416b847c9a68eed1377906070c65c261
Nur-Asyl <n-kanagatov@mail.ru>
2023-11-19 20:59:42 +0600

    request user validation crud

--- main.go
+++ main.go
@@ -13,8 +13,12 @@ import (
 )
 
 const dburi = "mongodb://localhost:27017"
-const dbname = "hotel-reservation"
-const userColl = "users"
+
+var config = fiber.Config{
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{"error": err.Error()})
+	},
+}
 
 func main() {
 	listenAddr := flag.String("listenAddr", ":3000", "The listen address of the API server")
@@ -27,10 +31,13 @@ func main() {
 
 	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
 
-	app := fiber.New()
+	app := fiber.New(config)
 
 	apiv1 := app.Group("api/v1")
 
+	apiv1.Put("/user/:id", userHandler.HandlePutUser)
+	apiv1.Delete("/user/:id", userHandler.HandleDeleteUser)
+	apiv1.Post("/user", userHandler.HandlePostUser)
 	apiv1.Get("/user", userHandler.HandleGetUsers)
 	apiv1.Get("/user/:id", userHandler.HandleGetUser)
 
